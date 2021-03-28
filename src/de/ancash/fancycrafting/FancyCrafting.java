ccfe5d9f86cce2d87fe49a44a9f12b9c44b8ae03
Ancash <bdspark2005@gmail.com>
2021-03-28 11:11:38 +0200

    fixed recipe editing not working

--- src/de/ancash/fancycrafting/FancyCrafting.java
+++ src/de/ancash/fancycrafting/FancyCrafting.java
@@ -2,13 +2,8 @@ package de.ancash.fancycrafting;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -19,9 +14,6 @@ import de.ancash.fancycrafting.gui.WorkbenchGUI;
 import de.ancash.fancycrafting.listeners.InventoryClickListener;
 import de.ancash.fancycrafting.listeners.WorkbenchClickListener;
 import de.ancash.fancycrafting.utils.IRecipe;
-import de.ancash.fancycrafting.utils.IShapedRecipe;
-import de.ancash.fancycrafting.utils.IShapelessRecipe;
-import de.ancash.ilibrary.datastructures.maps.CompactMap;
 import de.ancash.ilibrary.misc.FileUtils;
 import de.ancash.ilibrary.yaml.exceptions.InvalidConfigurationException;
 
@@ -52,24 +44,6 @@ public class FancyCrafting extends JavaPlugin{
 		PluginManager pm = Bukkit.getServer().getPluginManager();
 		pm.registerEvents(new InventoryClickListener(singleton), this);
 		pm.registerEvents(new WorkbenchClickListener(singleton), this);
-		
-		//The ingredients, key is slot in the crafting table grid
-		CompactMap<Integer, ItemStack> shapedIngredients = new CompactMap<>();
-		//insert ingredient
-		shapedIngredients.put(1, new ItemStack(Material.APPLE));
-		//new recipe (shaped)
-		IShapedRecipe shapedRecipe = new IShapedRecipe(shapedIngredients, new ItemStack(Material.GOLDEN_APPLE), UUID.randomUUID());
-		//register recipe
-		FancyCrafting.registerRecipe(shapedRecipe);
-		
-		//ingredients list (any collection)
-		List<ItemStack> shapelessIngredients = new ArrayList<ItemStack>();
-		shapelessIngredients.add(new ItemStack(Material.APPLE));
-		shapelessIngredients.add(new ItemStack(Material.APPLE));
-		//new recipe (shaped)
-		IShapelessRecipe shapelessRecipe = new IShapelessRecipe(new ItemStack(Material.GOLDEN_APPLE), shapelessIngredients, UUID.randomUUID());
-		//register recipe
-		FancyCrafting.registerRecipe(shapelessRecipe);
 		
 		getCommand("fc").setExecutor(new FancyCraftingCommand(this));
 	}
